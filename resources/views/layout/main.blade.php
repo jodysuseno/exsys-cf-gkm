43720b3b15f747a297b8e583db93c22b4b29f729
novelyaasis32 <novelya32@gmail.com>
2023-05-26 12:36:54 +0700

    revisi

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -24,11 +24,13 @@
   </head>
   <body class="app sidebar-mini">
     <!-- Navbar-->
-    <header class="app-header"><a class="app-header__logo" href="index.html">Exapp CBR</a>
+    <header class="app-header"><a class="app-header__logo" href="index.html">UPT PUSKESMAS SEKARGADUNG</a>
+  
       <!-- Sidebar toggle button-->
       <a class="app-sidebar__toggle" href="#" data-toggle="sidebar" aria-label="Hide Sidebar"></a>
       <!-- Navbar Right Menu-->
       <ul class="app-nav">
+      <!-- <img src="{{ ('img/logo.png ') }}"> -->
         <li class="dropdown"><a class="app-nav__item text-decoration-none" href="#" data-toggle="dropdown" aria-label="Open Profile Menu"><span>{{ auth()->user()->nama }}</span>&nbsp;&nbsp;<i class="fa fa-user fa-lg"></i></a>
           <ul class="dropdown-menu settings-menu dropdown-menu-right">
             {{-- <li><a class="dropdown-item" href="#"><i class="fa fa-cog fa-lg"></i> Settings</a></li>
